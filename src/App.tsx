19ee8d3be04ba2289dd4fb179b9b134a6e5fb290
LazyMG <cbfmark@gmail.com>
2023-09-10 22:31:33 +0900

    hash update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Routes/Home";
 import Tv from "./Routes/Tv";
 import Search from "./Routes/Search";
